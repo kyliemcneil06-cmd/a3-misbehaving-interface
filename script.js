4b5881ead874676707055c803831bfb3644a147f
kyliemcneil06-cmd <kyliemcneil06@gmail.com>
2026-05-25 15:25:03 -0400

    more spell checks

--- script.js
+++ script.js
@@ -101,11 +101,11 @@ function changeText() {
 }
 
 function Weee() {
-    document.getElementById("LETSGO").innerHTML = "Ok in order to take care of your son, you must give it enough heat to continue his development, take the slider and move it 100% to keep him warm!";
+    document.getElementById("LETSGO").innerHTML = "Ok in order to take care of your son, you must give it enough heat to continue his development! Take the slider and move it 100% to keep him warm!";
 }
 
 function batman() {
-    document.getElementById("justice").innerHTML = "hmmm the Son warmer 3000 didn't really do much exept make everything purple... oh well, mind use the Son Cooler 3001"; }
+    document.getElementById("justice").innerHTML = "hmmm the Son warmer 3000 didn't really do much except make everything purple... oh well, mind use the Son Cooler 3001"; }
 
 function YEAH() {
     document.getElementById("mhm").innerHTML = "Welp that doesn't seem to be working either... And this pesky bird won't go away! try to catch and click him and maybe he'll finally fly away!"; }
